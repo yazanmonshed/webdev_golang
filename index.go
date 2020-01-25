174021e942c593f63f4acff0e00a51b35e5b4093
Yazan Al Monshed <yazanmonshed@outlook.com>
2020-01-26 01:14:29 +0200

    first commit

---
+++ index.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+
+
+func main() {
+
+	http.HandleFunc("/", call)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func call(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Hello World!</h1>")
+}
